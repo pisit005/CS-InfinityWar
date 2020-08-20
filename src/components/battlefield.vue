b4c267d00b29355ce653a4a8be3d2ab9b59b481e
pisit005 <68525342+pisit005@users.noreply.github.com>
2020-08-20 15:57:06 +0700

    first

---
+++ src/components/battlefield.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="battle">
+        <h1>{{ mst }}</h1>
+        
+ 
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'battlefield',
+    props:{
+        mst: String
+    }
+    
+}
+</script>
+
+
+<style>
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
+import thanos from './components/thanos.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+//new Vue({
+//  render: h => h(App),
+//}).$mount('#app')
+
+new Vue()({
+  render: h => h(thanos),
+}).$mount('#Thanos')
